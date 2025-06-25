11a21b07089cfb50de90446bac52829a15da8c1d
rajesh <rk57893456@gmail.com>
2025-06-25 10:22:38 +0530

    MY C# program

---
+++ WebApplication1/Controllers/BeginnerController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BeginnerController : ControllerBase
+    {
+        private static readonly string[] Foods = { "Pizza", "Burger", "Dosa", "Pasta" };
+
+        [HttpGet("greet")]
+        public IActionResult Greet(string name, int age)
+        {
+            string message = $" Hello {name}, you are {age} years old.";
+            message += age < 18 ? " You're a minor." : " You're an adult.";
+            return Ok(new { message });
+        }
+
+        [HttpGet("menu")]
+        public IActionResult GetMenu()
+        {
+            return Ok(Foods.Select((food, index) => new { Id = index + 1, Name = food }));
+        }
+
+        [HttpGet("select")]
+        public IActionResult SelectFood(int choice)
+        {
+            if (choice < 1 || choice > Foods.Length)
+                return BadRequest("❌ Invalid choice!");
+
+            return Ok($"You selected: {Foods[choice - 1]}");
+        }
+    }
+}
